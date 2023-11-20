bc64f7697818a406c0d4b2bee7f643d941acd394
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2023-11-20 14:20:47 +0500

    mongodb atlas url and vercel.json file added

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const express = require('express')
-const morgan = require('morgan')
-const createError = require('http-errors')
-require('dotenv').config()
-require('./helpers/init_mongodb')
-const { verifyAccessToken } = require('./helpers/jwt_helper')
-require('./helpers/init_redis')
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+require("dotenv").config();
+require("./helpers/init_mongodb");
+const { verifyAccessToken } = require("./helpers/jwt_helper");
+require("./helpers/init_redis");
 
-const AuthRoute = require('./Routes/Auth.route')
+const AuthRoute = require("./Routes/Auth.route");
 
-const app = express()
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', verifyAccessToken, async (req, res, next) => {
-  res.send('Hello from express.')
-})
+app.get("/", verifyAccessToken, async (req, res, next) => {
+  res.send("Hello from express.");
+});
 
-app.use('/auth', AuthRoute)
+app.use("/auth", AuthRoute);
 
 app.use(async (req, res, next) => {
-  next(createError.NotFound())
-})
+  next(createError.NotFound());
+});
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500)
+  res.status(err.status || 500);
   res.send({
     error: {
       status: err.status || 500,
       message: err.message,
     },
-  })
-})
+  });
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
+
+// secure password  is    D7y7e48gbjIpMiA8
+// username is iqboljonsheraliyev11
--- helpers/init_mongodb.js
+++ helpers/init_mongodb.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -9,23 +9,23 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('mongodb connected.')
+    console.log("mongodb connected.");
   })
-  .catch((err) => console.log(err.message))
+  .catch((err) => console.log(err.message));
 
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to db')
-})
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to db");
+});
 
-mongoose.connection.on('error', (err) => {
-  console.log(err.message)
-})
+mongoose.connection.on("error", (err) => {
+  console.log(err.message);
+});
 
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose connection is disconnected.')
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose connection is disconnected.");
+});
 
-process.on('SIGINT', async () => {
-  await mongoose.connection.close()
-  process.exit(0)
-})
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
