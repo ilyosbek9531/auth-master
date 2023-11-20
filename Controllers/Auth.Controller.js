622740ad7b21914bc24cf4e7f9b7d4bc5341946f
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2023-11-20 15:30:59 +0500

    foxed

--- Controllers/Auth.Controller.js
+++ Controllers/Auth.Controller.js
@@ -1,18 +1,11 @@
 const createError = require("http-errors");
 const User = require("../Models/User.model");
 const { authSchema } = require("../helpers/validation_schema");
-const {
-  signAccessToken,
-  signRefreshToken,
-  verifyRefreshToken,
-} = require("../helpers/jwt_helper");
-const client = require("../helpers/init_redis");
+const { signAccessToken } = require("../helpers/jwt_helper");
 
 module.exports = {
   register: async (req, res, next) => {
     try {
-      // const { email, password } = req.body
-      // if (!email || !password) throw createError.BadRequest()
       const result = await authSchema.validateAsync(req.body);
 
       const doesExist = await User.findOne({ email: result.email });
@@ -24,9 +17,8 @@ module.exports = {
       const user = new User(result);
       const savedUser = await user.save();
       const accessToken = await signAccessToken(savedUser.id);
-      const refreshToken = await signRefreshToken(savedUser.id);
 
-      res.send({ accessToken, refreshToken });
+      res.send({ accessToken });
     } catch (error) {
       if (error.isJoi === true) error.status = 422;
       next(error);
@@ -44,44 +36,11 @@ module.exports = {
         throw createError.Unauthorized("Username/password not valid");
 
       const accessToken = await signAccessToken(user.id);
-      const refreshToken = await signRefreshToken(user.id);
 
-      res.send({ accessToken, refreshToken });
+      res.send({ accessToken });
     } catch (error) {
       if (error.isJoi === true)
         return next(createError.BadRequest("Invalid Username/Password"));
-      next(error);
-    }
-  },
-
-  refreshToken: async (req, res, next) => {
-    try {
-      const { refreshToken } = req.body;
-      if (!refreshToken) throw createError.BadRequest();
-      const userId = await verifyRefreshToken(refreshToken);
-
-      const accessToken = await signAccessToken(userId);
-      const refToken = await signRefreshToken(userId);
-      res.send({ accessToken: accessToken, refreshToken: refToken });
-    } catch (error) {
-      next(error);
-    }
-  },
-
-  logout: async (req, res, next) => {
-    try {
-      const { refreshToken } = req.body;
-      if (!refreshToken) throw createError.BadRequest();
-      const userId = await verifyRefreshToken(refreshToken);
-      client.DEL(userId, (err, val) => {
-        if (err) {
-          console.log(err.message);
-          throw createError.InternalServerError();
-        }
-        console.log(val);
-        res.sendStatus(204);
-      });
-    } catch (error) {
       next(error);
     }
   },
