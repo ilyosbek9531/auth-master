865ee0bb1eade2a7590bfeccb6c48d48e43c724e
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2023-11-20 15:22:09 +0500

    test

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -9,7 +9,7 @@ module.exports = {
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
         expiresIn: "1h",
-        issuer: "pickurpage.com",
+        issuer: "union.com",
         audience: userId,
       };
       JWT.sign(payload, secret, options, (err, token) => {
@@ -43,7 +43,7 @@ module.exports = {
       const secret = process.env.REFRESH_TOKEN_SECRET;
       const options = {
         expiresIn: "1y",
-        issuer: "pickurpage.com",
+        issuer: "union.com",
         audience: userId,
       };
       JWT.sign(payload, secret, options, (err, token) => {
