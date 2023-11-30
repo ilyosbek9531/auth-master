f923829bf5554abdd87841b5e5f69cc9fc155754
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2023-11-30 13:16:33 +0500

    fixed

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -1,6 +1,5 @@
 const JWT = require("jsonwebtoken");
 const createError = require("http-errors");
-const client = require("./init_redis");
 
 module.exports = {
   signAccessToken: (userId) => {
@@ -8,7 +7,7 @@ module.exports = {
       const payload = {};
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: "1h",
+        expiresIn: "8760h",
         issuer: "union.com",
         audience: userId,
       };
