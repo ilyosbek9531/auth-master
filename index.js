9a5bdfca79a95ceec43a22bf72e1b5be803fb08a
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2023-12-02 18:11:20 +0500

    fixed

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://auth-master.vercel.app"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
